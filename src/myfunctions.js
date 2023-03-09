bf94a2113c6d53f503682ec37d26f2ef9ee59d3c
Lennox Charles <lennyaiko17@gmail.com>
2023-03-09 18:26:12 +0100

    syntax for functions

---
+++ src/myfunctions.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var getWord = function (s) {
+    return "Contingency";
+};
+var heros = ["thor", "superman", "ironman"];
+heros.map(function (hero) {
+    return "hello Mr. ".concat(hero);
+});
---
+++ src/myfunctions.ts
@@ -0,0 +1,11 @@
+const getWord = (s: string): string => {
+    return "Contingency"
+}
+
+const heros = ["thor", "superman", "ironman"]
+
+heros.map((hero): string => {
+    return `hello Mr. ${hero}`
+})
+
+export {}
