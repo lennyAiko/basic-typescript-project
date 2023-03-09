81211156d5e74cd474ebe50876749ca0ba6f3fd2
Lennox Charles <lennyaiko17@gmail.com>
2023-03-09 20:19:13 +0100

    more syntax on types|

--- src/myobjects.js
+++ src/myobjects.js
@@ -5,7 +5,10 @@
 //     isActive: true 
 // }
 exports.__esModule = true;
-function createUser(user) {
-    return { name: "", email: "", isActive: true };
-}
-createUser({ name: "", email: "", isActive: true });
+var myUser = {
+    _id: "12345",
+    name: "l",
+    email: "l@l.dev",
+    isActive: false
+};
+myUser.email = "l@l.dev";
