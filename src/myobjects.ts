342d391d49cd389d9b5836dfc25a2821f284e3a2
Lennox Charles <lennyaiko17@gmail.com>
2023-03-09 19:01:34 +0100

    made use of type aliases

--- src/myobjects.ts
+++ src/myobjects.ts
@@ -1,20 +1,35 @@
-const User = {
-    name: "lennox",
-    email: "lennox@lco.dev",
-    isActive: true 
+// const User = {
+//     name: "lennox",
+//     email: "lennox@lco.dev",
+//     isActive: true 
+// }
+
+// function createUser({name: string, isPaid: boolean}) {}
+
+// createUser({name: "lennox", isPaid: false})
+
+// let newUser = {name: "lennox", isPaid: false, email: "h@h.dev"}
+
+// createUser(newUser)
+
+// // function name() returntype{} and object
+// function createCourse():{name: string, price: number}{
+//     return {name: "typescript", price: 3.99}
+// }
+
+// TYPE ALIASES
+
+// This really saves time and repitition
+type User = {
+    name: string;
+    email: string;
+    isActive: boolean
 }
 
-function createUser({name: string, isPaid: boolean}) {}
-
-createUser({name: "lennox", isPaid: false})
-
-let newUser = {name: "lennox", isPaid: false, email: "h@h.dev"}
-
-createUser(newUser)
-
-// function name() returntype{} and object
-function createCourse():{name: string, price: number}{
-    return {name: "typescript", price: 3.99}
+function createUser(user: User): User {
+    return {name: "", email: "", isActive: true}
 }
+
+createUser({name: "", email: "", isActive: true})
 
 export {}
