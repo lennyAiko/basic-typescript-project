d711e5c60d6ec12d125936175f69e129790785fd
Lennox Charles <lennyaiko17@gmail.com>
2023-03-12 19:25:52 +0100

    now work with arrays

---
+++ src/myarray.js
@@ -0,0 +1,7 @@
+var superHeroes = [];
+// const heroPower: number[] = []
+var heroPower = [];
+var allUsers = [];
+superHeroes.push("spiderman");
+heroPower.push(2);
+allUsers.push({ name: "", isActive: true });
