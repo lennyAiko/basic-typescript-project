961860a49afad942260d4df43c4d3cea3935474c
Lennox Charles <lennyaiko17@gmail.com>
2023-04-06 17:35:51 +0100

    made use of abstract classes

---
+++ pureTS/dist/abstractClass.js
@@ -0,0 +1,23 @@
+"use strict";
+class TakePhoto {
+    constructor(cameraMode, filter) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+    }
+    getReelTime() {
+        // complex calculation
+        return 9;
+    }
+}
+class Instagram extends TakePhoto {
+    constructor(cameraMode, filter, burst) {
+        super(cameraMode, filter);
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+    getSepia() {
+        console.log("sepia");
+    }
+}
+const lc = new Instagram("test", "Test", 3);
---
+++ pureTS/src/abstractClass.ts
@@ -0,0 +1,28 @@
+abstract class TakePhoto {
+    constructor(
+        public cameraMode: string,
+        public filter: string
+    ){}
+
+    abstract getSepia(): void
+    getReelTime(): number {
+        // complex calculation
+        return 9
+    }
+}
+
+class Instagram extends TakePhoto{
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public burst: number
+    ){
+        super(cameraMode, filter)
+    }
+
+    getSepia(): void {
+        console.log("sepia")
+    }
+}
+
+const lc = new Instagram("test", "Test", 3)
