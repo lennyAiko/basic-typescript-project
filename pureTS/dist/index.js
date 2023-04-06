79d0f663cd3155fc9f84b1cea8697e27e771e9b9
Lennox Charles <lennyaiko17@gmail.com>
2023-04-06 17:00:49 +0100

    classes

--- pureTS/dist/index.js
+++ pureTS/dist/index.js
@@ -1,2 +1,46 @@
 "use strict";
-console.log("typescript is here still");
+// class User {
+//     public email: string
+//     name: string
+//     private readonly city: string = "lagos"
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name;
+//         this.city
+//     }
+// }
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
+        this.city = "lagos";
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get getAppleEmail() {
+        return `apple ${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+}
+class subUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 4;
+    }
+}
+const lenny = new User("l@lc.com", "lenny");
+lenny.courseCount = 4;
+console.log(lenny.courseCount);
--- pureTS/src/index.ts
+++ pureTS/src/index.ts
@@ -1,2 +1,53 @@
-console.log("typescript is here still")
-console.log("typescript is amazing")
+// class User {
+//     public email: string
+//     name: string
+//     private readonly city: string = "lagos"
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name;
+//         this.city
+//     }
+// }
+
+
+class User {
+
+    protected _courseCount = 1
+
+    readonly city: string = "lagos"
+    constructor(
+        public email: string, 
+        public name: string,
+        // private userId: string
+    ){}
+
+    private deleteToken(){
+        console.log("Token deleted")
+    }
+    
+    get getAppleEmail(): string {
+        return `apple ${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1")
+        }
+        this._courseCount = courseNum
+    }
+}
+
+class subUser extends User {
+    isFamily: boolean = true
+    changeCourseCount(){
+        this._courseCount = 4
+    }
+}
+
+const lenny = new User("l@lc.com", "lenny")
+lenny.courseCount = 4
+console.log(lenny.courseCount)
